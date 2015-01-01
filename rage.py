a844c36ae9b5b8e824105660993573337b0861ce
tmr232 <tmr232@github>
2015-01-01 18:03:27 +0200

    Added path tracking for the keys.

--- rage.py
+++ rage.py
@@ -1,7 +1,7 @@
 from _winreg import *
 import types
 import collections
-
+import os.path
 
 
 """
@@ -17,11 +17,53 @@ def main():
 
 KeyInfo = collections.namedtuple("KeyInfo", "subkeys values modified")
 
+ROOT_NAMES = {
+    HKEY_CLASSES_ROOT: "HKEY_CLASSES_ROOT",
+    HKEY_CURRENT_USER: "HKEY_CURRENT_USER",
+    HKEY_LOCAL_MACHINE: "HKEY_LOCAL_MACHINE",
+    HKEY_CURRENT_CONFIG: "HKEY_CURRENT_CONFIG",
+    HKEY_USERS: "HKEY_USERS"
+}
+
+REVERSE_ROOT_NAMES = {value: key for key, value in ROOT_NAMES.iteritems()}
+
+
 class RegistryKey(object):
     def __init__(self, key, subkey=None):
+        if subkey is None:
+            subkey = ""
+
+        self._open_key(key, subkey)
+
+        base_path = self._get_key_path(key)
+        self._path = os.path.join(base_path, subkey)
+
+    def __repr__(self):
+        return "{}({})".format(
+            self.__class__.__name__,
+            repr(self.path)
+        )
+
+    def _open_key(self, key, subkey):
+        hkey = key
         if isinstance(key, RegistryKey):
-            key = key.key
-        self._key = OpenKey(key, subkey)
+            hkey = key.key
+
+        elif isinstance(key, types.StringTypes):
+            hkey, subkey = self._parse_key(key, subkey)
+        self._key = OpenKey(hkey, subkey)
+
+    def _get_key_path(self, key):
+        if isinstance(key, RegistryKey):
+            return key.path
+        elif isinstance(key, types.StringTypes):
+            return key
+        else:
+            return ROOT_NAMES[key]
+
+    @property
+    def path(self):
+        return self._path
 
     @property
     def key(self):
@@ -49,6 +91,23 @@ class RegistryKey(object):
         for index in xrange(subkeys):
             yield self._enum_key(index)
 
+    def _parse_key(self, key, subkey):
+        if not isinstance(key, types.StringTypes):
+            return key, subkey
+
+        base, path = os.path.split(key)
+        if base:
+            print base
+            hkey = REVERSE_ROOT_NAMES[base]
+            subkey_path = os.path.join(path, subkey)
+        else:
+            print path
+            hkey = REVERSE_ROOT_NAMES[path]
+            subkey_path = subkey
+
+        return hkey, subkey_path
+
+
 
 def enum_key_values(key):
     subkeys, values, modified = QueryInfoKey(key)
@@ -57,17 +116,19 @@ def enum_key_values(key):
 
 
 if __name__ == '__main__':
-
+    print REVERSE_ROOT_NAMES
     key = OpenKey(HKEY_CLASSES_ROOT, None)
     subkey = OpenKey(key, ".txt")
     print QueryInfoKey(key)
     print QueryInfoKey(subkey)
 
-    key = RegistryKey(HKEY_CLASSES_ROOT, None)
+    key = RegistryKey("HKEY_CLASSES_ROOT", None)
     subkey = key[".txt"]
     print key.get_info()
     print subkey.get_info()
 
+    print key
+    print subkey
 
     for value in subkey.enum_values():
         print value
